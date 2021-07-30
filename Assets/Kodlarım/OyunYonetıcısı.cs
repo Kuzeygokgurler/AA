e53f36de1cd8db495cc087aa18d68ba5858d83e2
Kuzeygokgurler <86321238+Kuzeygokgurler@users.noreply.github.com>
2021-07-30 12:12:02 +0300

    AA

---
+++ Assets/Kodlarım/OyunYonetıcısı.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class OyunYonetıcısı : MonoBehaviour
+{
+    GameObject donenCember;
+    GameObject AnaCember;
+    public Animator animator;
+    public Text DonenCemberLevel;
+    public Text bir;
+    public Text iki;
+    public Text uc;
+    public int KacTaneKucukCemberOlsun;
+    bool kontrol = true;
+
+    void Start()
+
+    {
+        PlayerPrefs.SetInt("kayit",int.Parse(SceneManager.GetActiveScene().name));
+        
+       
+        donenCember = GameObject.FindGameObjectWithTag("donencembertag");
+        AnaCember = GameObject.FindGameObjectWithTag("anacembertag");
+        DonenCemberLevel.text = SceneManager.GetActiveScene().name;
+
+        if (KacTaneKucukCemberOlsun < 2)
+        {
+            bir.text = KacTaneKucukCemberOlsun + "";
+        }
+        else if (KacTaneKucukCemberOlsun < 3)
+        {
+            bir.text = KacTaneKucukCemberOlsun + "";
+            iki.text = (KacTaneKucukCemberOlsun - 1) + "";
+        }
+        else
+        {
+            bir.text = KacTaneKucukCemberOlsun + "";
+            iki.text = (KacTaneKucukCemberOlsun - 1) + "";
+            uc.text = (KacTaneKucukCemberOlsun - 2) + "";
+        }
+        
+
+
+    }
+    
+    public void KucukCemberlerdeTextGosterme()
+    {
+        KacTaneKucukCemberOlsun--;
+        if (KacTaneKucukCemberOlsun < 2)
+
+        {
+            bir.text = KacTaneKucukCemberOlsun + "";
+            iki.text = "";
+            uc.text = "";
+        }
+        else if (KacTaneKucukCemberOlsun < 3)
+        {
+            bir.text = KacTaneKucukCemberOlsun + "";
+            iki.text = (KacTaneKucukCemberOlsun - 1) + "";
+            uc.text = "";
+        }
+        else
+        {
+            bir.text = KacTaneKucukCemberOlsun + "";
+            iki.text = (KacTaneKucukCemberOlsun - 1) + "";
+            uc.text = (KacTaneKucukCemberOlsun - 2) + "";
+        }
+        if (KacTaneKucukCemberOlsun == 0)
+        {
+            StartCoroutine(yeniLevel());
+
+        }
+
+    }
+    IEnumerator yeniLevel()
+    {
+
+        donenCember.GetComponent<Dondurme>().enabled = false;
+        AnaCember.GetComponent<AnaCember>().enabled = false;
+        yield return new WaitForSeconds(0.5f);
+        if (kontrol)
+        {
+            animator.SetTrigger("yenilevel");
+            yield return new WaitForSeconds(1.5f);
+
+            SceneManager.LoadScene(int.Parse(SceneManager.GetActiveScene().name) + 1);
+        }
+
+
+    }
+
+    
+   
+
+    public void OyunBitti()
+    {
+        StartCoroutine(CagrılanMetot());
+    }
+
+
+    IEnumerator CagrılanMetot()
+    {
+        donenCember.GetComponent<Dondurme>().enabled = false;
+        AnaCember.GetComponent<AnaCember>().enabled = false;
+        animator.SetTrigger("oyunbitti");
+        kontrol = false;
+
+        yield return new WaitForSeconds(1); // 1 saniye bekletiyoruz
+        
+
+        
+        SceneManager.LoadScene("AnaMenu");
+
+
+    }
+}
---
+++ Assets/menukontrol.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menukontrol : MonoBehaviour
+{
+    private void Start()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
+
+    public void oyunaGit()
+    {
+        int kayitlilevel= PlayerPrefs.GetInt("kayit");
+       
+       if (kayitlilevel == 0)
+       {
+            SceneManager.LoadScene(kayitlilevel + 1);
+       }
+
+        else
+        {
+            SceneManager.LoadScene(kayitlilevel);
+        }
+      
+
+    }
+    public void cik()
+    {
+        Application.Quit();
+    }
+}
